dbdb76be3003880aaadf5d5f65620984f54c6704
Hector2 <hecfran@gmail.com>
2024-06-29 15:48:54 +0200

    update on spanish and english

--- numbers/transcription.js
+++ numbers/transcription.js
@@ -7,24 +7,29 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+function getRandomWord(wordList) {
+    return wordList[Math.floor(Math.random() * wordList.length)];
+}
+
 function transcribeDigits() {
     const digits = document.getElementById('digits').value;
     const language = document.getElementById('language').value;
-    const dictionary = dictionaries[language].words;
-    
+    const dictionary = language === 'English' ? transcriptionDictEng : transcriptionDictSpa;
+
     let result = '';
     let i = 0;
     while (i < digits.length) {
-        if (i + 2 < digits.length && dictionary[digits.substr(i, 3)]) {
-            result += dictionary[digits.substr(i, 3)] + ' ';
-            i += 3;
-        } else if (i + 1 < digits.length && dictionary[digits.substr(i, 2)]) {
-            result += dictionary[digits.substr(i, 2)] + ' ';
-            i += 2;
-        } else if (dictionary[digits[i]]) {
-            result += dictionary[digits[i]] + ' ';
-            i++;
-        } else {
+        let found = false;
+        for (let len = Math.min(8, digits.length - i); len > 0; len--) {
+            const segment = digits.substr(i, len);
+            if (dictionary[segment]) {
+                result += getRandomWord(dictionary[segment]) + ' ';
+                i += len;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
             result += '[?] ';
             i++;
         }
